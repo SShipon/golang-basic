b8bdb2667903cfd10cd1a3230a24b4fa3f84b5a8
SShipon <nxshipon@gmail.com>
2025-01-08 07:45:02 +0600

    golang start

---
+++ frist.go
@@ -0,0 +1,7 @@
+
+ package main 
+ import ("fmt")
+ func main(){
+	var next = "golang start 1/7/2025"
+	fmt.Println(next)
+ }
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x float64 = 20.0
+    fmt.Println(x)
+    fmt.Printf("x is of type %T\n", x)
+    // Late Initialization
+    var num int
+    var amount float32
+    var isValid bool
+    var name string
+    num = 20
+    amount = 49.99
+    isValid = true
+    name = "Bappy"
+    fmt.Println(num, amount, isValid, name)
+}
